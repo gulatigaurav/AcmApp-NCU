174e2b545d286f74e654e28124467d71b2e4e7cc
gulatigaurav <gauravgulati9c@gmail.com>
2018-01-22 18:29:17 +0530

    Add Gallery page in views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,7 @@ class PagesController < ApplicationController
 
   def contact
   end
+
+  def gallery
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
 
   get '/contact', to: 'pages#contact'
 
+  get '/gallery', to: 'pages#gallery'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
